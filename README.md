f0088ac4a27ae4ea3810c7ad6e56504d4eede8a3
Petre Vane <PetreVane@users.noreply.github.com>
2020-04-24 19:40:25 +0200

    Added readme and source file

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # RemoteNotificationsConfigurator
 
-A description of this package.
+This package adds an extension to AppDelegate.
+The extension contains 2 methods, which ease the process of adding (remote) Notifications to any app.
+In order for this to work, you need a server (Vapor or any other server) which provides an API that saves device tokens in a PostgreSQL database.
+
+See chapter 6 in iOS Push Notifications book (Ray Wenderlich) to learn how to build that server with Vapor.
+Once you have your server running, implement these methods in AppDelegate:
+
+func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        registerForPushNotifications(application: application)
+        FirebaseApp.configure()
+       return true
+   }
+   
+   extension AppDelegate {
+   func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+       
+       sendPushNotification(to: "http://192.168.1.198:9000/api/token", withToken: deviceToken)
+       
+   }
--- Sources/RemoteNotificationsConfigurator/RemoteNotificationsConfigurator.swift
+++ Sources/RemoteNotificationsConfigurator/RemoteNotificationsConfigurator.swift
@@ -1,3 +1,47 @@
-struct RemoteNotificationsConfigurator {
-    var text = "Hello, World!"
+
+////
+////  AppDelegate+Notifications.swift
+////  FireChat
+////
+////  Created by Petre Vane on 24/04/2020.
+////  Copyright © 2020 Petre Vane. All rights reserved.
+////
+//
+import UIKit
+import UserNotifications
+
+
+public enum NotificationConfigurator {
+   
+    public static func registerForPushNotifications(application: UIApplication) {
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .badge, .sound, .provisional]) { (granted, _) in
+            guard granted else { return }
+            DispatchQueue.main.async {
+                application.registerForRemoteNotifications()
+            }
+        }
+    }
+    
+   public static func sendPushNotification(to stringURL: String, withToken deviceToken: Data) {
+        
+        guard let localURL = URL(string: stringURL) else { print("Invalid URL"); return }
+        let token = deviceToken.reduce("") { $0 + String(format: "%02x", $1) }
+        print("Device token is: \(token)")
+        var obj: [String: Any] = ["token": token, "debug": false]
+        
+        #if DEBUG
+            obj["debug"] = true
+            print("Device token is: \(token)")
+            let pretty = try! JSONSerialization.data( withJSONObject: obj, options: .prettyPrinted)
+            print(String(data: pretty, encoding: .utf8)!)
+        #endif
+        
+        var request = URLRequest(url: localURL)
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        request.httpBody = try! JSONSerialization .data(withJSONObject: obj)
+        URLSession.shared.dataTask(with: request).resume()
+    }
+    
 }
